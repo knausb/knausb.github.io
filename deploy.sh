145b40ce861fea2bd0f1f2ff1c7a04aab72acaea
Brian J. Knaus <briank.lists@gmail.com>
2017-05-23 13:09:58 -0700

    changed theme to Creative-portfolio

--- deploy.sh
+++ deploy.sh
@@ -34,4 +34,4 @@ git commit -m "$MESSAGE"
 git push origin "$SOURCE"
 
 msg "Pushing the updated \`public\` folder to the \`master\` branch"
-git subtree push --prefix=public master
+git subtree push --prefix=public origin master
